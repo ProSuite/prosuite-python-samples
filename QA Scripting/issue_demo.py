9d33e88368b3c666ca069eb940b01ee3746d9fc5
Patrick Franzelin <jpfranz20@gmail.com>
2024-09-17 16:46:04 +0200

    improve output messages

--- QA Scripting/issue_demo.py
+++ QA Scripting/issue_demo.py
@@ -12,11 +12,11 @@ import prosuite as ps
 
 # 2. Create & configure a Verification
 
-model = ps.Model("TopoModel", "D:\Test Data\ExtractStGallen.gdb") # From "ProSuite Documentation / SampleData.zip"
+model = ps.Model("TopoModel", "D:\Work\TestData\TLM\ExtractStGallen.gdb") # From "ProSuite Documentation / SampleData.zip"
 datasets = [ps.Dataset("TLM_FLIESSGEWAESSER", model),
             ps.Dataset("TLM_STRASSE", model)]
 
-service = ps.Service(host_name='localhost', port_nr=5151) # You might want to change this to the host and port of your ProSuite installation
+service = ps.Service(host_name='CRASSUS', port_nr=5151) # You might want to change this to the host and port of your ProSuite installation
 
 simpleSpecification = ps.Specification(
     name='MinimumLengthSpecification',
@@ -32,22 +32,22 @@ out_dir = 'C:/temp/verification_output' # You might want to change this to a dir
 
 verification_responses = service.verify(specification=simpleSpecification, output_dir=out_dir, perimeter=envelope)
 
-
 # 3. Run Verification and handle issues with the Issue Object
 
 issue_allowable = True
 
 for verification_response in verification_responses:
+    print(verification_response)
     if len(verification_response.issues) > 0:
         for issue in verification_response.issues:
             # Demo Prints
 
-            # print(issue.description)
-            # print(issue.involved_objects)
-            # print(issue.geometry)
-            # print(issue.issue_code)
-            # print(issue.allowable)
-            # print(issue.stop_condition)
+            print(issue.description)
+            print(issue.involved_objects)
+            print(issue.geometry)
+            print(issue.issue_code)
+            print(issue.allowable)
+            print(issue.stop_condition)
 
             if issue.allowable is False:
                 print(f"Not allowed issue met: {issue.description} in {issue.involved_objects[0].table_name}")
